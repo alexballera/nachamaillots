aceeb23d70b18572257c9738f72b617a78eb1259
Alex Ballera <alexballera@gmail.com>
2017-08-27 19:57:06 -0400

    terminado contacto

--- src/views/view3/template.js
+++ src/views/view3/template.js
@@ -4,8 +4,21 @@ import {NavItems} from '../../components/navigation'
 
 const elem = yo`
 <section id="seccion-${NavItems.id.id3}">
-  <div id="${NavItems.id.id3}">
-    <h1>Contacto</h1>
+  <div className="container" id="${NavItems.id.id3}">
+    <div className="row">
+      <h3 className="col s12 yellowtail-fonts">Contacto</h3>
+      <div className="s12">
+        <iframe
+          width="800"
+          height="650"
+          frameborder="0"
+          style="border:0"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDcxOoICiWQMHRKp8guwwf4RQ2xItWPXqw
+          &q=Calle+Santiago+Número+1,Utego+Zaragoza"
+          allowfullscreen>
+        </iframe>
+      </div>
+    </div>
   </div>
 </section>
 `
