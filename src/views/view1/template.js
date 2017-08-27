04ec1994bdd7286eaa3a762c852c143ebd9a0d8f
Alex Ballera <alexballera@gmail.com>
2017-08-27 01:22:44 -0400

    about

--- src/views/view1/template.js
+++ src/views/view1/template.js
@@ -7,9 +7,17 @@ const elem = yo`
   <div id="${NavItems.id.id1}" className="container">
     <h2>¿Quiénes Somos?</h2>
     <div className="row">
-      <div className="col s6"></div>
+      <div className="col s6">
+        <img src="" />
+      </div>
       <div className="col s6"></div>
     </div>
+    <div className="row">
+    <div className="col s6">
+      <img src="" />
+    </div>
+    <div className="col s6"></div>
+  </div>
   </div>
 </section>
 `
