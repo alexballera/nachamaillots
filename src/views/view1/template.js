c0981ce2ccad1032a52cdcd8a9fa3368d5369a2c
Alex Ballera <alexballera@gmail.com>
2017-08-27 01:22:25 -0400

    inicio de about

--- src/views/view1/template.js
+++ src/views/view1/template.js
@@ -4,8 +4,12 @@ import {NavItems} from '../../components/navigation'
 
 const elem = yo`
 <section id="seccion-${NavItems.id.id1}">
-  <div id="${NavItems.id.id1}">
-    <h1>¿Quiénes Somos?</h1>
+  <div id="${NavItems.id.id1}" className="container">
+    <h2>¿Quiénes Somos?</h2>
+    <div className="row">
+      <div className="col s6"></div>
+      <div className="col s6"></div>
+    </div>
   </div>
 </section>
 `
