dba2a4e0265e48c94eeb52431183aef5fa7b80d7
Alex Ballera <alexballera@gmail.com>
2017-09-17 09:10:25 -0400

    Mejorado diseño

---
+++ src/views/view2/data.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const data = {
+  section1: {
+    title: 'Maillots Vintage',
+    images: {
+      img1: {
+        dataCaption: 'Maillots Vintage',
+        src: 'images/modelo-maillots-1.png'
+      },
+      img2: {
+        dataCaption: 'Maillots Vintage',
+        src: 'images/modelo-maillots-1.png'
+      }
+    }
+  },
+  section2: {
+    title: 'Maillots Animal Print',
+    images: {
+      img1: {
+        dataCaption: 'Maillots Animal Print',
+        src: 'images/modelo-maillots-2.png'
+      },
+      img2: {
+        dataCaption: 'Maillots Animal Print',
+        src: 'images/modelo-maillots-2.png'
+      }
+    }
+  },
+  section3: {
+    title: 'Maillots Abstracto',
+    images: {
+      img1: {
+        dataCaption: 'Maillots Abstracto',
+        src: 'images/modelo-maillots-3.png'
+      },
+      img2: {
+        dataCaption: 'Maillots Abstracto',
+        src: 'images/modelo-maillots-3.png'
+      }
+    }
+  }
+}
+module.exports = data
--- src/views/view2/template.js
+++ src/views/view2/template.js
@@ -2,42 +2,50 @@
 import yo from 'yo-yo'
 import {NavItems} from '../../components/navigation'
 import {backgroundImageCatalogo} from '../../components/background-images'
+import data from './data.js'
 
 const elem = yo`
 <section id="seccion-${NavItems.id.id2}" className="container pink lighten-5">
   <div id="${NavItems.id.id2}">
     <h2 className="handlee-fonts">Diseños</h2>
     <div class="divider"></div>
-    <div className="row section">
-      <h3 className="col s12 yellowtail-fonts">Maillots Vintage</h3>
-      <div className="col s12 l3">
-        <img className="materialboxed" data-caption="Maillot" src="images/modelo-maillots-1.png" />
-      </div>
-      <div className="col s12 l3">
-        <img className="materialboxed" data-caption="Maillot" src="images/modelo-maillots-1.png" />
-      </div>
-    </div>
-    <div class="divider"></div>
-    <div className="row section">
-      <h3 className="col s12 yellowtail-fonts">Maillots Animal Print</h3>
-      <div className="col s12 l3">
-        <img className="materialboxed" data-caption="Maillot Animal" src="images/modelo-maillots-2.png" />
-      </div>
-      <div className="col s12 l3">
-        <img className="materialboxed" data-caption="Maillot Animal" src="images/modelo-maillots-2.png" />
+    <div className="section">
+      <h3 className="col s12 yellowtail-fonts">${data.section1.title}</h3>
+      <div className="row images-container">
+        <div className="col s12 l4">
+          <img className="materialboxed" data-caption=${data.section1.images.img1.dataCaption} src=${data.section1.images.img1.src} />
+        </div>
+        <div className="col s12 l4">
+          <img className="materialboxed" data-caption=${data.section1.images.img2.dataCaption} src=${data.section1.images.img2.src} />
+        </div>
       </div>
     </div>
     <div class="divider"></div>
-    <div className="row section">
-      <h3 className="col s12 yellowtail-fonts">Maillots Abstracto</h3>
-      <div className="col s12 l3">
-        <img className="materialboxed" data-caption="Maillot Abstracto" src="images/modelo-maillots-3.png" />
-      </div>
-      <div className="col s12 l3">
-        <img className="materialboxed" data-caption="Maillot Abstracto" src="images/modelo-maillots-3.png" />
+
+    <div className="section">
+      <h3 className="col s12 yellowtail-fonts">${data.section2.title}</h3>
+      <div className="row images-container">
+        <div className="col s12 l4">
+          <img className="materialboxed" data-caption=${data.section2.images.img1.dataCaption} src=${data.section2.images.img1.src} />
+        </div>
+        <div className="col s12 l4">
+          <img className="materialboxed" data-caption=${data.section2.images.img2.dataCaption} src=${data.section2.images.img2.src} />
+        </div>
       </div>
     </div>
     <div class="divider"></div>
+
+    <div className="section">
+      <h3 className="col s12 yellowtail-fonts">${data.section3.title}</h3>
+      <div className="row images-container">
+        <div className="col s12 l4">
+          <img className="materialboxed" data-caption=${data.section3.images.img1.dataCaption} src=${data.section3.images.img1.src} />
+        </div>
+        <div className="col s12 l4">
+          <img className="materialboxed" data-caption=${data.section3.images.img2.dataCaption} src=${data.section3.images.img2.src} />
+        </div>
+      </div>
+    </div>
     ${backgroundImageCatalogo}
   </div>
 </section>
